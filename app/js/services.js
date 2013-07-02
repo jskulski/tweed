2e0306c35b4fa65d3bcf52e3fbc717ade5788286
jskulski-trulia <jskulski@trulia.com>
2013-07-01 23:13:53 -0700

    text model + test

--- app/js/services.js
+++ app/js/services.js
@@ -17,6 +17,7 @@ app.factory('Text', [function() {
   Text.cursor = 0;
 
   Text.setText = function TextSetText(text) {
+    Text.cursor = 0;
     Text.original = text;
     Text.chopped = text.split(' ');
     Text.chopMap = Text.createChopMap(Text.chopped);
@@ -28,17 +29,17 @@ app.factory('Text', [function() {
    * @return {array} [description]
    */
   Text.createChopMap = function TextCreateChopMap(choppedText) {
-    var map = [],
-        bucket = [],
-        last = 0,
-        groupSize = 6; // TODO: determine best size
+    var map = [];
+    var last = 0;
+    var bucket = [];
+    var bucketSize = 6; // TODO: user input
 
     choppedText.forEach(function(item, index) {
       // Put items in the bucket until it grows to the max size
       bucket.push(item);
 
       // When its full, empty bucket and add that range to the bucket
-      if (bucket.length > groupSize) {
+      if (bucket.length > bucketSize) {
         map.push({start: last, end: index})
         bucket = [];
         last = index + 1;
@@ -49,6 +50,48 @@ app.factory('Text', [function() {
     map.push({start: last, end: choppedText.length});
 
     return map;
+  };
+
+  /**
+   * Safely scrolls to a specific phrase
+   * @param {integer} pos Position of phrase to scroll to
+   * @returns {boolean} unless you are out of bounds
+   */
+  Text.scrollTo = function TextScrollTo(phrase) {
+    var inBounds = (phrase >= 0 && phrase < Text.chopMap.length);
+    if (inBounds) {
+      Text.cursor = phrase;
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  /**
+   * Moves cursor forward
+   * @returns Text true unless you are at the last phrase
+   */
+  Text.next = function TextNext() {
+    return Text.scrollTo(Text.cursor + 1);
+  };
+
+  /**
+   * Moves cursor backwards
+   * @returns {boolean} true unless you are the first phrase
+   */
+  Text.prev = function TextPrev() {
+    return Text.scrollTo(Text.cursor - 1);
+  };
+
+  /**
+   * Gets the current text phrase
+   * @returns {array} The phrase as an array of words
+   */
+  Text.current = function TextGetCurrent() {
+    var range = Text.chopMap[Text.cursor];
+    var current = Text.chopped.slice(range.start, range.end);
+    return current;
   };
 
   return Text;
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -4,9 +4,9 @@
 
 describe('service', function() {
 
-  var scope,
-      defaultText = 'This is my text and this is how I expect it to work.',
-      defaultChop = defaultText.split(' ');
+  var scope;
+  var defaultText = 'This is my text and this is how I expect it to work.';
+  var defaultChop = defaultText.split(' ');
 
   beforeEach(module('tweed.services'));
 
@@ -26,23 +26,50 @@ describe('service', function() {
       expect(Text.chopped).toEqual(defaultChop);
     }));
 
+    it('should set the cursor to zero after the text changes', inject(function(Text) {
+      expect(Text.cursor).toBe(0);
+      Text.next();
+      Text.setText('changing text');
+      expect(Text.cursor).toBe(0);
+    }))
+
     it('the map can be used to transverse the text and regain the original', inject(function(Text) {
-      var part = [],
-          parts = [],
-          whole = '',
-          chopped = Text.chopped,
-          map = Text.chopMap;
+      var part = [];
+      var parts = [];
+      var whole = '';
+      var chopped = Text.chopped;
+      var map = Text.chopMap;
 
       map.forEach(function(range) {
         part = chopped.slice(range.start, range.end + 1);
         parts.push(part.join(' '));
       });
-      console.log(parts);
       whole = parts.join(' ');
 
       expect(Text.original).toEqual(whole);
+    }))
+
+    it('should give me the current phrase', inject(function(Text) {
+      var text = '';
+      text = Text.current().join(' ');
+      expect(text).toEqual('This is my text and this');
+    }))
+
+    it('should scroll and give me the next phase', inject(function(Text) {
+      var text = '';
+      Text.next();
+      text = Text.current().join(' ');
+      expect(text).toEqual('how I expect it to work.');
+      expect(Text.cursor).toBe(1);
     }));
 
+    it('should not scroll out of bounds', inject(function(Text) {
+      var success = Text.scrollTo(-1);
+      expect(success).toBeFalsy();
+
+      var success = Text.scrollTo(Text.chopMap.length + 1);
+      expect(success).toBeFalsy();
+    }));
 
   });
 });
